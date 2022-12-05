89f0cfb9c4727b7cfa1e733692e5e72f128d8899
Thiago Pontes <github@thiago.me>
2022-12-05 15:19:25 -0500

    Add day5

---
+++ day5/2.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+
+stacks = {}
+input = [line for line in sys.stdin]
+mid_point = input.index("\n")
+stacks_data, moves = input[:mid_point], input[mid_point + 1 :]
+for id in stacks_data.pop().strip().split():
+    stacks[id] = deque()
+
+j = 0
+k = 3
+curr_stack = 1
+while k < len(max(stacks_data)):
+    for i in range(len(stacks_data)):
+        item = stacks_data[i][j:k]
+        if item.strip() != "":
+            stacks[str(curr_stack)].append(item)
+    curr_stack += 1
+    j = k + 1
+    k += 4
+
+for move in moves:
+    command = move.strip().split()
+    qnt, orig, dest = int(command[1]), command[3], command[5]
+    mover = deque()
+    make_moves = qnt
+    while make_moves > 0:
+        mover.appendleft(stacks[orig].popleft())
+        make_moves -= 1
+    while qnt > 0:
+        stacks[dest].appendleft(mover.popleft())
+        qnt -= 1
+print(
+    "".join(stacks[k].popleft() for k in stacks.keys())
+)
